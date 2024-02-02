453d88ed6263db3965e03dbd89b2c32acbbe2e2b
slicycode <deparisjules180@gmail.com>
2024-02-02 22:10:42 +0100

    Getting Started

---
+++ src/app/(main)/agency/(auth)/layout.tsx
@@ -0,0 +1,7 @@
+const AuthLayout = ({ children }: React.PropsWithChildren) => {
+  return (
+    <div className="h-full flex justify-center items-center">{children}</div>
+  )
+}
+
+export default AuthLayout
