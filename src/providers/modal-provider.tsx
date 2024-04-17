336dc64d225470cd99972fdfef7555118b9b5143
slicycode <deparisjules180@gmail.com>
2024-04-17 15:34:05 +0200

    fix: build deploy issue

--- src/providers/modal-provider.tsx
+++ src/providers/modal-provider.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { PricesList, TicketDetails } from "@/libs/types";
+// import { PricesList, TicketDetails } from "@/libs/types";
 import { Agency, Contact, Plan, User } from "@prisma/client";
 import { createContext, useContext, useEffect, useState } from "react";
 
@@ -11,11 +11,11 @@ interface ModalProviderProps {
 export type ModalData = {
   user?: User;
   agency?: Agency;
-  ticket?: TicketDetails[0];
+  // ticket?: TicketDetails[0];
   contact?: Contact;
   plans?: {
     defaultPriceId: Plan;
-    plans: PricesList["data"];
+    // plans: PricesList["data"];
   };
 };
 
